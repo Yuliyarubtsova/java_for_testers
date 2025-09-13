02721effeaf7ee513ec07a9bd7c9a81155135871
Yuliyarubtsova <yuliya.rubcova89@mail.ru>
2025-09-13 19:25:54 +0300

    Внесение изменений в методы

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
@@ -8,5 +10,10 @@ public class Hello {
 
         System.out.println("hello, " + "world!");
         System.out.println("2 + 2 = " + (2 + 2));
+
+        var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
+
     }
 }
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,17 +1,34 @@
 package ru.stqa.geometry.figures;
 
 public class Square {
-    public static void printSquareArea(double side) {
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, Area(side));
+
+    public double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public static void printSquareArea(Square s) {
+        String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
 
     }
+//    public static double area(double a) {
+//        return a * a;
+//    }
 
-    public static double Area(double a) {
-        return a * a;
+//    public static double perimeter(double a) {
+//        return 4 * a;
+//    }
+
+    public double area() {
+        return this.side * this.side;
     }
 
-    public static double perimeter(double a) {
-        return 4 * a;
+    public double perimeter() {
+        return 4 * this.side;
     }
 }
+
+
+
