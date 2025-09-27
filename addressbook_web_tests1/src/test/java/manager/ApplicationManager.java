2808d16c5027522a8cf97faadaa28bbcc1b2d3e1
Yuliyarubtsova <yuliya.rubcova89@mail.ru>
2025-09-27 17:49:18 +0300

    Добавление GroupHelper, LoginHelper

---
+++ addressbook_web_tests1/src/test/java/manager/ApplicationManager.java
@@ -0,0 +1,48 @@
+package manager;
+
+import model.GroupData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ApplicationManager {
+    protected static WebDriver driver;
+    private LoginHelper session;
+    private GroupHelper groups;
+
+    public void init() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(757, 691));
+            session().login("admin", "secret");
+        }
+    }
+
+    public GroupHelper groups() {
+        if (groups == null) {
+            groups = new GroupHelper(this);
+        }
+        return groups;
+    }
+
+    public LoginHelper session() {
+        if (session == null) {
+            session = new LoginHelper(this);
+        }
+        return session;
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+
+}
