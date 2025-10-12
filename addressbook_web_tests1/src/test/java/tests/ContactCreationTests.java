d4a4cf95c795ffbf2858ec4b7f3c9144e206226d
Yuliyarubtsova <yuliya.rubcova89@mail.ru>
2025-10-12 18:20:59 +0300

    Доработка теста по созданию групп -  задание 10

--- addressbook_web_tests1/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests1/src/test/java/tests/ContactCreationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase{
@@ -42,10 +43,18 @@ public class ContactCreationTests extends TestBase{
     @ParameterizedTest
     @MethodSource("contactProvider")
     public void canCreateMultipleContacts(ContactData contact) {
-        int contactCount = app.contacts().getCountContacts();
+        var oldContacts = app.contacts().getList();
         app.contacts().createContact(contact);
-        int newContactCount = app.contacts().getCountContacts();
-        Assertions.assertEquals(contactCount + 1, newContactCount);
+        var newContacts = app.contacts().getList();
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newContacts.sort(compareById);
+
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.add(contact.withId(newContacts.get(newContacts.size() - 1).id()).withAddress("").withMobile("").withEmail("").withHomepage(""));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newContacts, expectedList);
     }
     @Test
     public void createContacts() {
